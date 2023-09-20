7276fddeabc81298f85fb49ac4054b0e3a05fb13
ToniGrbic <tonigrbic.5@gmail.com>
2023-09-20 12:28:34 +0200

    minor formatting fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/Footer.module.scss";
 import { SocialLinks } from "./";
 
 const Footer = () => {
-  const [year, setYear] = useState < string > "";
+  const [year, setYear] = useState<string>("");
 
   useEffect(() => {
     const currentYear = new Date().getFullYear().toString();
