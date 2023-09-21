583ecc5d55500abb377c3d82388d20d3f5def502
ToniGrbic <tonigrbic.5@gmail.com>
2023-09-21 14:46:48 +0200

    added title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Layout } from "../components";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>tonigrbic portfolio</title>
+      </head>
       <body>
         <Layout>{children}</Layout>
       </body>
