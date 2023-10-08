0cf9bc812c508482a6aa78d530f95264c2fb3c42
ToniGrbic <tonigrbic.5@gmail.com>
2023-10-08 16:20:03 +0200

    social links open to new tab

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -4,17 +4,29 @@ import { AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from 'react-icons/ai
 const SocialLinks = () => {
   return (
     <>
-          <a href='https://github.com/ToniGrbic'> 
-            <AiFillGithub size={30}/>
-          </a>
-          <a href='https://www.linkedin.com/in/toni-grbi%C4%87-6aa509249/'>
-            <AiFillLinkedin size={30}/> 
-          </a>
-          <a href='https://www.instagram.com/toni.grbic/'>
-            <AiOutlineInstagram size={30}/>
-          </a>
+      <a
+        href="https://github.com/ToniGrbic"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillGithub size={30} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/toni-grbi%C4%87-6aa509249/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillLinkedin size={30} />
+      </a>
+      <a
+        href="https://www.instagram.com/toni.grbic/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiOutlineInstagram size={30} />
+      </a>
     </>
-  )
+  );
 }
 
 export default SocialLinks
