4bdb503d0977a3d60ad205f166be5f3e36ecc9e7
ToniGrbic <tonigrbic.5@gmail.com>
2023-10-29 14:37:40 +0100

    added metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "../styles/globals.scss";
 import { Layout } from "../components";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "tonigrbic portfolio",
+  description: " my portfolio website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>tonigrbic portfolio</title>
-      </head>
       <body>
-        <Layout>
-          {children}
-        </Layout>
+        <Layout>{children}</Layout>
       </body>
     </html>
   );
