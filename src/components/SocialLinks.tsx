f5bc6575d0ad65b080352578e601043fde214942
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-11 00:23:32 +0100

    changed layout

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -5,33 +5,49 @@ import {
   AiOutlineInstagram,
 } from "react-icons/ai";
 
-const SocialLinks = () => {
+const SocialLinks = ({ showIconText = false }: { showIconText?: boolean }) => {
   return (
     <>
-      <a
-        aria-label="github"
-        href="https://github.com/ToniGrbic"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <AiFillGithub size={30} />
-      </a>
-      <a
-        aria-label="linkedin"
-        href="https://www.linkedin.com/in/toni-grbi%C4%87-6aa509249/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <AiFillLinkedin size={30} />
-      </a>
-      <a
-        aria-label="instagram"
-        href="https://www.instagram.com/toni.grbic/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <AiOutlineInstagram size={30} />
-      </a>
+      <div>
+        <a
+          aria-label="github"
+          href="https://github.com/ToniGrbic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="social-link-container">
+            <AiFillGithub size={30} />
+
+            {showIconText ? <p className="social-link-text">Github</p> : ""}
+          </div>
+        </a>
+      </div>
+      <div>
+        <a
+          aria-label="linkedin"
+          href="https://www.linkedin.com/in/toni-grbi%C4%87-6aa509249/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="social-link-container">
+            <AiFillLinkedin size={30} />
+            {showIconText ? <p className="social-link-text">Linkden</p> : ""}
+          </div>
+        </a>
+      </div>
+      <div>
+        <a
+          aria-label="instagram"
+          href="https://www.instagram.com/toni.grbic/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="social-link-container">
+            <AiOutlineInstagram size={30} />
+            {showIconText ? <p className="social-link-text">Instagram</p> : ""}
+          </div>
+        </a>
+      </div>
     </>
   );
 };
