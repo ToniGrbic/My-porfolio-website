5d9d89f2fae61d81a608c196c7094f56c5ae7a8e
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-12 14:28:37 +0100

    navlinks export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
-export { default as Footer} from './Footer'
-export { default as Layout} from './Layout'
-export { default as Navbar} from './Navbar'
+export { default as Footer } from "./Footer";
+export { default as Layout } from "./Layout";
+export { default as Navbar } from "./Navbar";
 export { default as SideBar } from "./SideBar";
-export { default as Modal} from './Modal'
-export { default as SocialLinks} from './SocialLinks'
-export { default as Project} from './Project'
+export { default as Modal } from "./Modal";
+export { default as SocialLinks } from "./SocialLinks";
+export { default as NavLinks } from "./NavLinks";
+export { default as Project } from "./Project";
